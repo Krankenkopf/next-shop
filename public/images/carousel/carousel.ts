b857ca10959c0bfa0b14130a3f3068861b61d8f8
Krankenkopf <petricowork@gmail.com>
2021-10-25 05:17:56 +0300

    /assets - imgs managed

---
+++ public/images/carousel/carousel.ts
@@ -0,0 +1,19 @@
+import a from "./1001.png"
+import b from "./1002.png"
+import c from "./1003.png"
+import d from "./1004.png"
+import e from "./1005.png"
+import f from "./1006.png"
+import g from "./1007.png"
+import j from "./1008.png"
+import i from "./1009.png"
+import k from "./1010.png"
+import l from "./1011.png"
+import m from "./1012.png"
+import n from "./1013.png"
+import o from "./1014.png"
+import p from "./1015.png"
+import q from "./1016.png"
+
+// T_T
+export const images = [a.src, b.src, c.src, d.src, e.src, f.src, g.src, j.src, i.src, k.src, l.src, m.src, n.src, o.src, p.src, q.src]
