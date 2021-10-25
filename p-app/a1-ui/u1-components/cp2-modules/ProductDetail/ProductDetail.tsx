b2138b456baff8e1e421d4063ab1e71bdcfa0649
Krankenkopf <petricowork@gmail.com>
2021-10-25 05:18:26 +0300

    lost commit

--- p-app/a1-ui/u1-components/cp2-modules/ProductDetail/ProductDetail.tsx
+++ p-app/a1-ui/u1-components/cp2-modules/ProductDetail/ProductDetail.tsx
@@ -1,7 +1,21 @@
-export const ProductDetail = () => {
+import { FC } from "react"
+
+type TProductDetail = {
+    code:       string
+    name:       string
+    price:      string
+    imgSrc:     string
+}
+
+export const ProductDetail: FC<TProductDetail> = (props) => {
+    const {code, name, price, imgSrc} = props
     return (
         <>
-            
+            <div>{name}</div>
+            <div>
+                <img src={imgSrc} alt={name} />
+            </div>
+            <div>{ price}</div>
         </>
     )
 }
