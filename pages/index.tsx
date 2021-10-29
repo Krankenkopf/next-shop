78896f5961bd897219a79e2f06b2189f660ea989
Krankenkopf <petricowork@gmail.com>
2021-10-30 02:40:23 +0300

    /pages - landing page expanded

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,39 @@ import React, { MouseEventHandler, useState } from "react";
 import Button from "../p-app/a1-ui/u1-components/cp1-elements/el02-Button/Button";
 import { Timer } from "../p-app/a1-ui/u1-components/cp1-elements/el20-Timer/Timer";
 import { LandingLayout } from "../p-app/a1-ui/u1-components/cp4-layouts/LandingLayout";
-import campaign from "../public/images/campaign.jpg"
+import campaign01 from "../public/images/campaign01.jpg"
+import campaign02 from "../public/images/campaign02.jpg"
+import campaign03 from "../public/images/campaign03.jpg"
 import { images } from "../public/images/carousel/carousel"
-import test from "../public/images/carousel/10201.png"
+import magazine01 from "../public/images/magazine01.jpg"
+import magazine02 from "../public/images/magazine02.jpg"
+import magazine03 from "../public/images/magazine03.jpg"
 
 export default function Index({ history, props }: any) {
     console.log(history);
     const carouselSources = [
-        { title: "", p: "", src: images[0]},
-        { title: "", p: "", src: images[1]},
-        { title: "", p: "", src: images[2]},
-        { title: "", p: "", src: images[3]},
-        { title: "", p: "", src: images[4]},
-        { title: "", p: "", src: images[5]},
-        { title: "", p: "", src: images[6]},
-        { title: "", p: "", src: images[7]},
-        { title: "", p: "", src: images[8]},
-        { title: "", p: "", src: images[9]},
-        { title: "", p: "", src: images[10]},
-        { title: "", p: "", src: images[11]},
-        { title: "", p: "", src: images[12]},
-        { title: "", p: "", src: images[13]},
-        { title: "", p: "", src: images[14]},
-        { title: "", p: "", src: images[15]},
+        { title: "", p: "", src: images[0] },
+        { title: "", p: "", src: images[1] },
+        { title: "", p: "", src: images[2] },
+        { title: "", p: "", src: images[3] },
+        { title: "", p: "", src: images[4] },
+        { title: "", p: "", src: images[5] },
+        { title: "", p: "", src: images[6] },
+        { title: "", p: "", src: images[7] },
+        { title: "", p: "", src: images[8] },
+        { title: "", p: "", src: images[9] },
+        { title: "", p: "", src: images[10] },
+        { title: "", p: "", src: images[11] },
+        { title: "", p: "", src: images[12] },
+        { title: "", p: "", src: images[13] },
+        { title: "", p: "", src: images[14] },
+        { title: "", p: "", src: images[15] },
     ]
     const [stage, setStage] = useState('0');
     const setStageHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         setStage(e.currentTarget.value)
     }
-    
+
     const carousel = carouselSources.map((item) => {
         return (
             <li key={item.src} className="carousel__item__container">
@@ -39,7 +43,7 @@ export default function Index({ history, props }: any) {
                     <a>
                         <div className="carousel__item">
                             <div className="carousel__imgContainer">
-                                <img src={item.src} alt="carouselImg" /> 
+                                <img src={item.src} alt="carouselImg" />
                             </div>
                             <h5>{item.title}</h5>
                             <p>{item.p}</p>
@@ -51,99 +55,173 @@ export default function Index({ history, props }: any) {
     })
     return (
         <LandingLayout>
-            <div className="wrapper _container">
-                <section id="unique_propositions" className="section-container">
+            <section id="unique_propositions" className="section-container">
+                <ul>
+                    <li><Link href="/">
+                        <a>
+                            Free shipping over $40: Select Expedited, Standard Ship to Store or UPS Access Point™ at checkout.
+                        </a>
+                    </Link></li>
+                    <li><Link href="/">
+                        <a>Become a Loyalty Member. Join for Free!
+                        </a>
+                    </Link></li>
+                    <li><Link href="/">
+                        <a>
+                            Gift Card
+                        </a>
+                    </Link></li>
+                </ul>
+            </section>
+            <section id="banner" className="section-container">
+                <Link href="/">
+                    <a>
+                        <div>
+                            <h3 className="banner__title">
+                                Member perk: 20% off $75 + free shipping
+                            </h3>
+                            <div>
+                                TIME FLIES:
+                            </div>
+                            <div>
+                                <Timer />
+                            </div>
+                        </div>
+                    </a>
+                </Link>
+            </section>
+            <section id="campaign">
+                <Link href="/">
+                    <a>
+                        <div>
+                            <img src={campaign01.src} alt="campaign01"></img>
+                        </div>
+                        <div></div>
+                        <div className="campaign__block">
+                            <h2>Cozy Combinashnz</h2>
+                            <p>Soft, layerable knits to see you through winter</p>
+                            <div>
+                                <Button>SCHOP NAU!</Button>
+                            </div>
+                        </div>
+                    </a>
+                </Link>
+            </section>
+            <section id="carousel"> {/*HARDCODED to 16 items, no responsive!!!*/}
+                <h3>Trendin' rait nau</h3>
+                <div className="carousel" style={{ transform: `translateX(${-100 * (+stage)}%)` }} >
                     <ul>
-                        <li><Link href="/">
-                            <a>
-                                Free shipping over $40: Select Expedited, Standard Ship to Store or UPS Access Point™ at checkout.
-                            </a>
-                        </Link></li>
-                        <li><Link href="/">
-                            <a>Become a Loyalty Member. Join for Free!
-                            </a>
-                        </Link></li>
-                        <li><Link href="/">
-                            <a>
-                                Gift Card
-                            </a>
-                        </Link></li>
+                        {carousel}
                     </ul>
-                </section>
-                <section id="banner" className="section-container">
-                    <Link href="/">
-                        <a>
+                </div>
+                <div className="carousel__controls">
+                    <ul>
+                        <li><button value={'0'}
+                            onClick={setStageHandler}
+                            style={stage === "0" ? { backgroundColor: "#E50010", borderColor: "#E50010" } : undefined}
+                            className="carousel__controls__button" /></li>
+                        <li><button value={'1'}
+                            onClick={setStageHandler}
+                            style={stage === "1" ? { backgroundColor: "#E50010", borderColor: "#E50010" } : undefined}
+                            className="carousel__controls__button" /></li>
+                        <li><button value={'1.333333333'}
+                            onClick={setStageHandler}
+                            style={stage === "1.333333333" ? { backgroundColor: "#E50010", borderColor: "#E50010" } : undefined}
+                            className="carousel__controls__button" /></li>
+                    </ul>
+                </div>
+            </section>
+            <section id="banner" className="section-container">
+                <Link href="/">
+                    <a>
+                        <div>
+                            <h3 className="banner__title">
+                                Let us upgrade you! Become an H&amp;M Member &amp; get 10% off your first purchase + rewards just for shopping
+                            </h3>
+                            <p>
+                                Don't forget to opt into Fashion News to have your offers and rewards delivered right to your inbox!
+                            </p>
+                        </div>
+                    </a>
+                </Link>
+            </section>
+            <section id="campaign">
+                <Link href="/">
+                    <a>
+                        <div>
+                            <img src={campaign02.src} alt="campaign02"></img>
+                        </div>
+                        <div></div>
+                        <div className="campaign__block">
+                            <h2>The best kind of basic</h2>
+                            <p>Just-in hoodies, joggers, sweatshirts &amp; more</p>
                             <div>
-                                <h3 className="banner__title">
-                                    Member perk: 20% off $75 + free shipping
-                                </h3>
-                                <div>
-                                    TIME FLIES:
-                                </div>
-                                <div>
-                                    <Timer />
-                                </div>
+                                <Button>SCHOP NAU!</Button>
                             </div>
-                        </a>
-                    </Link>
-                </section>
-                <section id="campaign">
-                    <Link href="/">
-                        <a>
+                        </div>
+                    </a>
+                </Link>
+            </section>
+            <section id="campaign">
+                <Link href="/">
+                    <a>
+                        <div>
+                            <img src={campaign03.src} alt="campaign03"></img>
+                        </div>
+                        <div></div>
+                        <div className="campaign__block">
+                            <h2>The best kind of basic</h2>
+                            <p>Just-in hoodies, joggers, sweatshirts &amp; more</p>
                             <div>
-                                <img src={campaign.src} alt="campaign01"></img>
+                                <Button>SCHOP NAU!</Button>
                             </div>
-                            <div></div>
-                            <div className="campaign__block">
-                                <h2>Cozy Combinashnz</h2>
-                                <p>Soft, layerable knits to see you through winter</p>
-                                <div>
-                                    <Button>SCHOP NAU!</Button>
-                                </div>
-                            </div>
-                        </a>
+                        </div>
+                    </a>
+                </Link>
+            </section>
+            <section id="magazine">
+                <div>
+                    <h2>MAGAZINE</h2>
+                    <h6>A WORLD OF INSPIRATION</h6>
+                    <Link href="/">
+                        <a className="dashed">READ H&M MAGAZINE</a>
                     </Link>
-                </section>
-                <section id="carousel">
-                    <h3>Trendin' rait nau</h3>
-                    <div className="carousel" style={{transform: `translateX(${-100*(+stage)}%)`}} >
+                    <div className="magazine__articles">
                         <ul>
-                            {carousel}
+                            <li>
+                                <Link href="/"><a>
+                                   <div className="magazine__imgContainer">
+                                        <img src={magazine01.src} alt="magazine01" />
+                                    </div>
+                                    <h5>INSIDE H&M</h5>
+                                    <h4>Presenting: Edition by<br/>John Boyega</h4>
+                                    <span>READ THE EX₡ITIN' STORI →</span>
+                                </a></Link>                               
+                            </li>
+                            <li>
+                            <Link href="/"><a>
+                                   <div className="magazine__imgContainer">
+                                        <img src={magazine02.src} alt="magazine02" />
+                                    </div>
+                                    <h5>INSIDE H&M</h5>
+                                    <h4>Designed by</h4>
+                                    <span>READ THE EX₡ITIN' STORI →</span>
+                                </a></Link>                               
+                            </li>
+                            <li>
+                            <Link href="/"><a>
+                                   <div className="magazine__imgContainer">
+                                        <img src={magazine03.src} alt="magazine03" />
+                                    </div>
+                                    <h5>INSIDE H&M</h5>
+                                    <h4>Coming this spring:<br/>Iris Apfel x H&M</h4>
+                                    <span>READ THE EX₡ITIN' STORI →</span>
+                                </a></Link>                               
+                            </li>
                         </ul>
                     </div>
-                    <div className="carousel__controls">
-                        <ul>
-                            <li><button value={'0'}
-                                onClick={setStageHandler}
-                                style={stage === "0" ? {backgroundColor: "#E50010", borderColor: "#E50010"} : undefined}
-                                className="carousel__controls__button" /></li>
-                            <li><button value={'1'}
-                                onClick={setStageHandler}
-                                style={stage === "1" ? {backgroundColor: "#E50010", borderColor: "#E50010"} : undefined}
-                                className="carousel__controls__button" /></li>
-                            <li><button value={'2'}
-                                onClick={setStageHandler}
-                                style={stage === "2" ? {backgroundColor: "#E50010", borderColor: "#E50010"} : undefined}
-                                className="carousel__controls__button" /></li>
-                        </ul>
-                    </div>
-                </section>
-                <section>
-                    <div>
-                        <Link href="/posts">
-                            <a>Posts</a>
-                        </Link>
-                    </div>
-                    <div>
-                        <Link href="/about">
-                            About
-                        </Link>
-                    </div>
-                </section>
-                <h1>Main</h1>
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
+                </div>
+            </section>
         </LandingLayout>
-
     )
 }
