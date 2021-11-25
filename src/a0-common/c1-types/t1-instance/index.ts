f9e65983d495dafd0f4f73a04d9c963a476f1e4c
Krankenkopf <petricowork@gmail.com>
2021-11-26 01:44:28 +0300

    /types - necessary typings added

---
+++ src/a0-common/c1-types/t1-instance/index.ts
@@ -0,0 +1,24 @@
+export type Nullable<T> = T | null
+
+export type TPageMeta = {
+    title: string
+    path: TRootCategoryValue
+}
+
+export type TCategory = {
+    CatName: string
+    CategoryValue: string
+    CategoriesArray?: Array<TCategory>
+    tagCodes: string[]
+}
+
+export type TRootCategory<V extends TRootCategoryValue, C extends TRootCategoryTagCodes> = {
+    CatName: string
+    CategoryValue: V
+    CategoriesArray?: Array<TCategory>
+    tagCodes: C
+}
+
+export type TRootCategoryValue = "ladies" | "divided" | "men" | "baby" | "kids" | "home" | "sale"
+type TRootCategoryTagCodes = [] | [
+    "ladies_all" | "ladies_divided" | "men_all" | "kids_newbornbaby_viewall" | "kids_all" | "home_all"]
