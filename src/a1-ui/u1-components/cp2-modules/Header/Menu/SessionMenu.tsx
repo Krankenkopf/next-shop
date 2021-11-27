5388961445d187a0770c4850bfb0381c05997354
Krankenkopf <petricowork@gmail.com>
2021-11-27 04:23:04 +0300

    /auth/menu - authed state case added

--- src/a1-ui/u1-components/cp2-modules/Header/Menu/SessionMenu.tsx
+++ src/a1-ui/u1-components/cp2-modules/Header/Menu/SessionMenu.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link"
-import React, { FC, useState } from "react"
+import React, { FC, useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { logout, TAuthState } from "../../../../../a2-bll/auth-reducer"
+import { TState } from "../../../../../a2-bll/store"
 import Button from "../../../cp1-elements/el02-Button/Button"
 import { Icon } from "../../../cp1-elements/el10-Icons/Icon"
 import { DropMenu } from "../../DropMenu/DropMenu"
@@ -12,36 +15,69 @@ type TSessionMenuProps = {
 }
 
 export const SessionMenu: FC<TSessionMenuProps> = ({ revealModal }) => {
-    const authMenu = {
-        toggle: <a onClick={() => revealModal("signup")}>
-            <div className="link__decorated">
-                <Icon name="user" className="icon__session" />
-                <span>Sign Up</span>
-            </div>
-        </a>,
-        menu: <div className="session">
-            <div className="button-container">
-                <Button variant="ok" onClick={() => revealModal("signup")}>
-                    Sign up
-                </Button>
-            </div>
-            <ul>
-                <li>
-                    <Link href="/"><a><span>Loyalty Program Info</span></a></Link>
-                </li>
-                <li style={{ fontSize: "90%" }}>
-                    <a onClick={() => revealModal("signup")}>
-                        <em>Not a Member yet? Join here!</em>
-                    </a>
-                </li>
-            </ul>
-        </div>
+    const dispatch = useDispatch()
+    const { isLoggedIn } = useSelector<TState, TAuthState>((state) => state.auth)
+    const handleLogout = () => {
+        dispatch(logout())
     }
+    const authMenu = isLoggedIn
+        ? {
+            toggle: <Link href="/account">
+                <a>
+                    <div className="link__decorated">
+                        <Icon name="user-check" className="icon__session" />
+                        <span>My Account</span>
+                    </div>
+                </a>
+            </Link>,
+            menu: <div className="session-auth">
+                <ul className="logged-in">
+                    <li>
+                        <Link href="/account"><a><span>My Account</span></a></Link>
+                    </li>
+                    <li>
+                        <Link href="/"><a><span>Loyalty Program Info</span></a></Link>
+                    </li>
+                    <li style={{ fontSize: "90%" }}>
+                        <a onClick={handleLogout}>
+                            <em>Sign out</em>
+                        </a>
+                    </li>
+                </ul>
+            </div>,
+        }
+        : {
+            toggle: <a onClick={() => revealModal("login")}>
+                <div className="link__decorated">
+                    <Icon name="user" className="icon__session" />
+                    <span>Sign In</span>
+                </div>
+            </a>,
+            menu: <div className="session-auth">
+                <div className="button-container">
+                    <Button variant="ok" onClick={() => revealModal("login")}>
+                        Sign in
+                    </Button>
+                </div>
+                <ul>
+                    <li>
+                        <Link href="/"><a><span>Loyalty Program Info</span></a></Link>
+                    </li>
+                    <li style={{ fontSize: "90%" }}>
+                        <a onClick={() => revealModal("signup")}>
+                            <em>Not a Member yet? Join here!</em>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        }
     const minicartMenu = {
-        toggle: <Link href="/">
+        toggle: <Link href="/cart">
             <a>
                 <div className="link__decorated">
-                    <Icon name="cart-shopping-fast" className="icon__session" />
+                    {isLoggedIn
+                        ? <Icon name="cart-shopping" className="icon__session" />
+                        : <Icon name="cart-shopping-fast" className="icon__session" />}
                     <span>Cart</span>
                 </div>
             </a>
@@ -50,18 +86,20 @@ export const SessionMenu: FC<TSessionMenuProps> = ({ revealModal }) => {
     }
     return (
         <ul className="header-menu-session">
-            <li style={{ display: "inline-block"}}>
+            <li style={{ display: "inline-block" }}>
                 <DropMenu toggle={authMenu.toggle} menu={authMenu.menu} />
             </li>
             <li style={{ display: "inline-block" }}>
-                <a onClick={() => revealModal("login")}>
-                    <div className="link__decorated" >
-                        <Icon name="heart" className="icon__session" />
-                        <span>Favorites</span>
-                    </div>
-                </a>
+                <Link href="/favorites">
+                    <a>
+                        <div className="link__decorated" >
+                            <Icon name="heart" className="icon__session" />
+                            <span>Favorites</span>
+                        </div>
+                    </a>
+                </Link>
             </li>
-            <li style={{ display: "inline-block"}}>
+            <li style={{ display: "inline-block" }}>
                 <DropMenu toggle={minicartMenu.toggle} menu={minicartMenu.menu} />
             </li>
         </ul>
