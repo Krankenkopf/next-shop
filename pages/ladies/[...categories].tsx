e42b12b058aa26ddcc956dfe35a48fe7692860b1
Krankenkopf <petricowork@gmail.com>
2021-11-29 04:22:23 +0300

    /pages - logic for getting props unificated for all similar products subcategories

--- pages/ladies/[...categories].tsx
+++ pages/ladies/[...categories].tsx
@@ -1,146 +1,106 @@
-import Router, { useRouter } from "next/router"
+import Router, { NextRouter, useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
 import { Nullable, TCategory, TPageMeta, TRootCategoryValue } from "../../src/a0-common/c1-types/t1-instance"
 import { CountryCodes, LanguageCodes } from "../../src/a0-common/c1-types/t1-instance/regions"
 import { TProduct } from "../../src/a0-common/c1-types/t1-instance/TProduct"
 import { TGetProductsListRequestRequiredData, TGetProductsListRequestOptionalData } from "../../src/a0-common/c1-types/t2-request"
-import { useAppSelector } from "../../src/a0-common/c3-hooks"
-import { ProductItem } from "../../src/a1-ui/u1-components/cp2-modules/ProductItem/ProductItem"
+import { useAppDispatch, useAppSelector } from "../../src/a0-common/c3-hooks"
+import { getRequestedCategory } from "../../src/a0-common/c4-utils"
+import { Preloader } from "../../src/a1-ui/u1-components/cp1-elements/el11-Preloader/Preloader"
+import { ProductItem } from "../../src/a1-ui/u1-components/cp2-modules/ProductModules/ProductItem/ProductItem"
+import { ProductsList } from "../../src/a1-ui/u1-components/cp2-modules/ProductModules/ProductsList/ProductsList"
 import { ProductLayout } from "../../src/a1-ui/u1-components/cp4-layouts/ProductLayout"
+import { TRequestStatus } from "../../src/a2-bll/app-reducer"
+import { setCategory, TNavigationState } from "../../src/a2-bll/navigation-reducer"
+import { clearProducts, getProducts, setProducts } from "../../src/a2-bll/products-reducer"
 import { selectPageCategory, selectPageMeta } from "../../src/a2-bll/selectors"
-import { wrapper } from "../../src/a2-bll/store"
+import { AppThunkDispatch, wrapper } from "../../src/a2-bll/store"
 import { ProductsAPI } from "../../src/a3-dal/hm/products-api"
 
 type TLadiesCategorySSProps = {
-    products: Nullable<Array<TProduct>>
+    categorySS?: TCategory
+    productsSS: Nullable<Array<TProduct>>
 }
 type TLadiesCategoryProps = {
     history: Array<string>
 }
 
-export default function LadiesCategory(props: TLadiesCategorySSProps & TLadiesCategoryProps) {
-    const category = useAppSelector<TCategory>(state => selectPageCategory(state, "ladies"))
+export default function LadiesCategory({categorySS, productsSS, history}: TLadiesCategorySSProps & TLadiesCategoryProps) {
+    const dispatch = useAppDispatch()
+    const { pathname, query, asPath } = useRouter()
+    const status = useAppSelector<TRequestStatus>(state => state.app.status)
+    const rootCategory = useAppSelector<TCategory>(state => selectPageCategory(state, "ladies"))
     const pageMeta = useAppSelector<TPageMeta>(state => selectPageMeta(state, "ladies"))
-
-    const [products, setProducts] = useState<Array<TProduct> | null>(props.products)
-
-    const router = useRouter()
+    const {currentPage, category, pageSize, numberOfPages, totalNumberOfResults} = useAppSelector<TNavigationState>(state => state.navigation)
+    const products = useAppSelector<Nullable<Array<TProduct>>>(state => state.products.products)
+    //const [products, setProducts] = useState<Array<TProduct> | null>(productsSS)
+    
     useEffect(() => {
-        async function onLoad() {
-            const queryCategories = router.query.categories as Array<string>
-            let targetedCategory: TCategory | undefined
-            if (queryCategories) {
-                let currentBranch = category as TCategory
-                for (const queryCategory of queryCategories) {
-                    targetedCategory = currentBranch?.CategoriesArray?.find(category => {
-                        if (category.CategoryValue === queryCategory) {
-                            if (category.CategoriesArray) {
-                                currentBranch = category // nested category will be processed in next cycle
-                            }
-                            return category
-                        }
-                    })
-                }
-            }
-            //const res = await fetch("http://localhost:4200/results")
-            const requiredParams: TGetProductsListRequestRequiredData = {
-                country: CountryCodes.United_States,
-                lang: LanguageCodes.English,
-                currentpage: 0,
-                pagesize: 50,
-            }
-            const optionalParams: TGetProductsListRequestOptionalData = {
-                categories: targetedCategory?.tagCodes
-            }
-            //const response = await fetch("http://localhost:4200/results")
-            const response = await ProductsAPI.getList(requiredParams, optionalParams)
-            const products = response.data.results
-            //const products = await response.json()
-            setProducts(products)
+        const onLoad = async () => {
+            const queryCategories = query.categories as Array<string>
+            dispatch(getProducts(pathname, queryCategories))
         }
-        if (!props.products) {
+        if (!productsSS) {
             console.log("client");
             onLoad()
         }
-    }, [])
+        return () => {dispatch(clearProducts)}
+    }, [asPath])
 
-    if (!props.products) {
-        // TODO: preloader
-    }
     // console.log(products);
-
-    const mappedProducts = products && products.map((item: TProduct, i) => {
-        return <ProductItem key={item.code}
-            code={item.articles[0].code}
-            name={item.name}
-            price={item.price.formattedValue}
-            imgSrc={item.images[0].url}
-            rgbColors={item.rgbColors}
-            articleColorNames={item.articleColorNames}
-            sellingAttributes={item.sellingAttributes} />
-    })
+    
     return (
         <ProductLayout title={pageMeta.title}
-            category={category}
+            category={rootCategory}
             rootCategoryName={pageMeta.path}>
             <div className="page-content">
                 <div>
                     <h3>{pageMeta.title}</h3>
                 </div>
-                <div className="section">
-                    <ul>
-                        {mappedProducts}
+                <div className="products-list__container">
+                    <ul className="products-list">
+                        {products && <ProductsList products={ products }/>}
                     </ul>
+                    <Preloader isVisible={status === "loading"}/>
                 </div>
             </div>
         </ProductLayout>
     )
 }
 
-export const getServerSideProps = wrapper.getServerSideProps(store => async ({ req, query, resolvedUrl }) => {
+export const getServerSideProps = wrapper.getServerSideProps<TLadiesCategorySSProps>(store => async ({ req, query, resolvedUrl }) => {
     if (!req || (req.url && req.url.startsWith('/_next/data'))) {
-        return { props: { products: null } }
+        console.log("ss request dumped");
+        
+        return { props: { productsSS: null } }
     }
     try {
-        // parsing url and find according category in state to form request params
-        const rootCategory = resolvedUrl.split("/")[1] as TRootCategoryValue //exm. "ladies"
-        const queryCategories = query.categories as Array<string> //exm. ["new", "view-all"]
-        const categoriesBranch = store.getState().categories[rootCategory] //exm. ladies: {TCategory}
-        let targetedCategory: TCategory | undefined
-        if (queryCategories) {
-            let currentBranch = categoriesBranch as TCategory
-            for (const queryCategory of queryCategories) {
-                targetedCategory = currentBranch?.CategoriesArray?.find(category => {
-                    if (category.CategoryValue === queryCategory) {
-                        if (category.CategoriesArray) {                   
-                            currentBranch = category // nested category will be processed in next cycle
-                        }
-                        return category
-                    }
-                })
-            }
-        }
-  
+        const state = store.getState()
+        const queryCategories = query.categories as Array<string>
+        const targetedCategory = getRequestedCategory(resolvedUrl, queryCategories, state.categories)
+        const region = state.regions
+        const navigation = state.navigation
+
         const requiredParams: TGetProductsListRequestRequiredData = {
-            country: CountryCodes.United_States,
-            lang: LanguageCodes.English,
-            currentpage: 0,
-            pagesize: 50,
+            country: region.country,
+            lang: region.lang,
+            currentpage: navigation.currentPage,
+            pagesize: navigation.pageSize,
         }
         const optionalParams: TGetProductsListRequestOptionalData = {
             categories: targetedCategory?.tagCodes
         }
-
-        //const response = await fetch("http://localhost:4200/results")
-        const response = await ProductsAPI.getList(requiredParams, optionalParams)
-        console.log("ss request");
-
-        const products = response.data.results
-        //console.log(products)
-        //const products = await response.json() as Nullable<Array<TProduct>>
-        return { props: { products } }
+        const response = await fetch("http://localhost:4200/results")
+        const productsSS = await response.json() as Nullable<Array<TProduct>>
+        //const response = await ProductsAPI.getList(requiredParams, optionalParams)
+        console.log("ss request")
+        //const productsSS = response.data.results
+        productsSS && store.dispatch(setProducts(productsSS))
+        targetedCategory && store.dispatch(setCategory(targetedCategory))
+        
+        return { props: { productsSS, categorySS: targetedCategory } }
     } catch (e) {
         console.log(e);
-        return { props: { products: null } }
+        return { props: { productsSS: null } }
     }
 })
--- src/a0-common/c3-hooks/index.ts
+++ src/a0-common/c3-hooks/index.ts
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { AppDispatch, TState } from '../../a2-bll/store'
+import { AppThunkDispatch, TState } from '../../a2-bll/store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
 export const useAppSelector: TypedUseSelectorHook<TState> = useSelector
