eb0875b4a0e3533ee2a7df30a73f728ef69e57d9
Krankenkopf <petricowork@gmail.com>
2021-12-04 13:53:31 +0300

    /dropmenu - nestable using added

--- src/a1-ui/u1-components/cp2-modules/DropMenu/DropMenuOnClick.tsx
+++ src/a1-ui/u1-components/cp2-modules/DropMenu/DropMenuOnClick.tsx
@@ -1,30 +1,61 @@
-import React, { FC, ReactNode, useCallback, useState } from "react"
+
+import React, { FC, ReactNode, useCallback, useState, MouseEvent, useEffect } from "react"
 import { useOnMouseDownOutside } from "../../../../a0-common/c3-hooks"
 import css from "./DropMenu.module.scss"
 type TDropMenuOnClickProps = {
     toggle: ReactNode
     menu: ReactNode
-    onToggle?: (state: boolean) => void
+    isNeedToClosePrevious?: boolean
+    id?: string
+    className?: string
+    type?: string
+    onToggle?: (state: boolean, type?: string) => void
 }
 
-export const DropMenuOnClick: FC<TDropMenuOnClickProps> = ({ toggle, menu, onToggle }) => { 
+export const DropMenuOnClick: FC<TDropMenuOnClickProps> = ({ toggle, menu, isNeedToClosePrevious = false, id, className, type, onToggle }) => {
     const [isMenuVisible, setIsMenuVisible] = useState(false)
-
+    const [isToggling, setIsToggling] = useState(false);
+    // TODO: comment this barbarity!!
     const onToggleClick = useCallback(() => {
-        setIsMenuVisible(!isMenuVisible)
-        onToggle && onToggle(!isMenuVisible)
-    }, [isMenuVisible])
+        setIsToggling(false)
+        if (!isToggling) {
+            setIsMenuVisible(!isMenuVisible)
+            onToggle && onToggle(!isMenuVisible, type ? type : undefined)
+        }
+        
+    }, [isMenuVisible, isToggling])
+    useEffect(() => {
+        if (isToggling) {
+            setTimeout(() => {
+                setIsMenuVisible(!isMenuVisible)
+                onToggle && onToggle(!isMenuVisible, type ? type : undefined) 
+            },0)
+            
+        }
+    }, [isToggling])
     const dropmenu = useOnMouseDownOutside(() => {
         isMenuVisible && setIsMenuVisible(false)
-        onToggle && onToggle(false)
+        onToggle && onToggle(false, type ? type : undefined)
     })
-
-    return <div className={css.dropdown__container}
-        onClick={onToggleClick}
-        onMouseDown={e => e.stopPropagation()}>
-        {/* this ^ blocks onMouseDownOutside event*/}
-        {toggle}
-        <div ref={dropmenu}
+    console.log(isNeedToClosePrevious);
+    
+    const onToggleMouseDown = (e: MouseEvent) => { // blocks onMouseDownOutside event
+        if (!isNeedToClosePrevious) {
+            e.stopPropagation()
+        }
+        if (isNeedToClosePrevious) {
+            setIsToggling(true)
+        }      
+    }
+    const box = `${css.dropdown__container} ${className ? className : ""}`
+    // 
+    return <div className={box}>
+        <div id={id} onClick={onToggleClick}
+            onMouseDown={onToggleMouseDown}
+            style={{padding: "5px"}}>
+            {toggle}
+        </div>
+        <div  ref={dropmenu}
             className={isMenuVisible
                 ? css.dropdown__menu
                 : `${css.dropdown__menu} ${css._closed}`}>
