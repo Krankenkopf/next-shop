af7a2b9c98ac8e84eb290148886ac95353065f59
Krankenkopf <petricowork@gmail.com>
2021-12-04 13:54:44 +0300

    /elements/checkbox - deeply refactored

--- src/a1-ui/u1-components/cp1-elements/el03-Checkbox/Checkbox.tsx
+++ src/a1-ui/u1-components/cp1-elements/el03-Checkbox/Checkbox.tsx
@@ -1,50 +1,55 @@
 import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
-import classes from './Checkbox.module.css'
+import css from './Checkbox.module.scss'
 
 
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 type ExtraCheckboxPropsType = DefaultInputPropsType & {
-    onChangeChecked?: (checked: boolean) => void
-    spanClassName?: string
+    boxClassName?: string
+    titleClassName?: string
+
+    onChangeChecked?: (checked: boolean, value: string) => void
 }
 
 export const Checkbox: React.FC<ExtraCheckboxPropsType> = (
     {
+        name,
         type,
+        disabled = false,
         onChange, onChangeChecked,
-        className, spanClassName,
+        className, titleClassName,
         alt,
         children,
         ...restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.currentTarget.checked) {
-            onChangeChecked && onChangeChecked(e.currentTarget.checked)
-            onChange && onChange(e)
-        }
+        onChangeChecked && onChangeChecked(e.currentTarget.checked, e.currentTarget.value)
+        onChange && onChange(e)
+
     }
 
-    const finalInputClassName = `${classes.innerCbx}`
-    const finalBoxClassName = `${classes.checkboxBox} ${alt && !restProps.checked ? alt : ''}`
+    const box = `${css.checkbox} ${className ? className : ''}`
+    const title = `${css.title} ${titleClassName ? titleClassName : ''}`
 
     return (
-        <label className={finalBoxClassName}>
+        <>
             <input
                 type={'checkbox'}
+                disabled={disabled}
                 onChange={onChangeCallback}
-                className={finalInputClassName}
-                id="cbx"
+                id={name}
+                className={css.input}
                 {...restProps}
             />
-            <label htmlFor="cbx" className={classes.check}>
-                <svg width="22px" height="22px" viewBox="0 0 18 18">
-                    <path
-                        d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"> </path>
-                    <polyline points="1 9 7 14 15 4"> </polyline>
-                </svg>
+            <label htmlFor={name} >
+                <div className={box}>
+                    <svg width="22px" height="22px" viewBox="0 0 18 18">
+                        <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"> </path>
+                        <polyline points="1 9 7 14 15 4"> </polyline>
+                    </svg>
+                    {children && <div className={title}>{children}</div>}
+                </div>
             </label>
-            {children && <div className={classes.spanClassName}>{children}</div>}
-        </label>
+        </>
     )
 }
